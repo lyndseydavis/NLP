4df056ad3a2f3ec1230fe1a5dcca9eb0e58a3116
Lyndsey <lyndsey_davis2@baylor.edu>
2022-04-04 13:20:38 -0500

    part 1 - bar chart

--- nlp_assignment.py
+++ nlp_assignment.py
@@ -5,21 +5,33 @@ from textblob import TextBlob
 from pathlib import Path
 import pandas as pd
 from operator import itemgetter
+import matplotlib.pyplot as plt
 
 from nyc_trends import nyc_trends
 
 tweets = nyc_trends[0]["trends"]
-print(len(tweets))
+# print(len(tweets))
 
-volume = tweets[0]["tweet_volume"]
-print(volume)
+volume, topic = [], []
+for tw in range(len(tweets)):
+    vol = tweets[tw]["tweet_volume"]
+    volume.append(vol)
+# print(volume)
 
-"""
-list_of_tweets = nyc_trends["trends"]
-print(len(list_of_tweets))
+for tw in range(len(tweets)):
+    name = tweets[tw]["name"]
+    topic.append(name)
+# print(topic)
+volume_topic_dict = {"Topic": topic, "Volume": volume}
+# print(volume_topic_dict)
+volume_topic_df = pd.DataFrame(volume_topic_dict)
+# print(volume_topic_df)
 
-
-sorted_text = sorted(tweet_volume)
-top10 = sorted_text[:10]
+sorted_df = volume_topic_df.sort_values("Volume", ascending=False)
+top10 = sorted_df[:10]
+df = pd.DataFrame(top10, columns=["Topic", "Volume"])
 print(top10)
-"""
+
+df.plot.bar(x="Topic", y="Volume", legend=False)
+plt.gcf().tight_layout()  # need to fix this???
+plt.show()
